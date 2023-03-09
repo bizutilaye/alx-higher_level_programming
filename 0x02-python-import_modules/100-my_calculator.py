88c2455b50bacf7a584f56af554069f7f0706be4
bizutilaye <bizualemtilaye@gmail.com>
2023-03-09 13:27:15 -0800

    Update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     """Handle basic operations."""
-    
+
     # Check if the number of arguments is 3
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
