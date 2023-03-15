552d3671c2ca0a48d3e32ee370e62678c8f7eede
bizutilaye <bizualemtilaye@gmail.com>
2023-03-15 12:16:57 -0700

    Update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
+    best_key = None
     best_value = flot('-inf')
     for key, value in a_dictionary.items():
         if value > best_value:
+            beest_key = key
             best_value == value
-    return best_value
+    return best_key
