daef750cc004295944f2709b5c7a3f0250a04a50
bizutilaye <bizualemtilaye@gmail.com>
2023-03-15 12:21:22 -0700

    Update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,13 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
+
     best_key = None
-    best_value = flot('-inf')
+    best_value = float("-inf")
+
     for key, value in a_dictionary.items():
         if value > best_value:
             best_key = key
-            best_value == value
+            best_value = value
+
     return best_key
