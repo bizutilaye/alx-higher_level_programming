1707174b33f31bce8ef61c09c168ad79087132c1
bizutilaye <Bizualemtilaye@gmail.com>
2023-03-27 10:05:50 -0700

    Update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    n = 0
-    try:
-        while n < x:
-            print("{}".format(my_list[n]), end="")
-            n += 1
-    except:
-        pass
+    printed = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            printed += 1
+        except:
+            break
     print()
-    return n
+    return printed
