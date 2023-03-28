920fbb5d1ec008e5c41d39b4a88f977adb9d1e23
bizutilaye <Bizualemtilaye@gmail.com>
2023-03-28 11:16:02 -0700

    update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
+"""
+This module contains a class Square that defines a square.
+"""
+
+
 class Square:
     """
-    Square: a class that represents a square.
+    A class that defines a square.
 
     Attributes:
-        size (int): The size of the square.
+        __size (int): The size of the square.
     """
-
     def __init__(self, size=0):
         """
-        Initializes a square object with a given size.
+        Initializes a square.
 
         Args:
             size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
         """
-        Getter method for the size attribute.
+        Gets the size of the square.
 
         Returns:
             int: The size of the square.
@@ -29,25 +37,24 @@ class Square:
     @size.setter
     def size(self, value):
         """
-        Setter method for the size attribute.
+        Sets the size of the square.
 
         Args:
             value (int): The size of the square.
 
         Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     def area(self):
         """
-        Calculates and returns the area of the square.
+        Computes the area of the square.
 
         Returns:
             int: The area of the square.
