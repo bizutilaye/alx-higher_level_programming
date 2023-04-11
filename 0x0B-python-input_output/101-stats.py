b64f8ef32b5aab8a15db9a869d9299a84466271f
bizutilaye <Bizualemtilaye@gmail.com>
2023-04-11 06:18:40 -0700

    Update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,31 +1,38 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 import sys
 
-total_file_size = 0
-status_codes_count = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 
-try:
-    for i, line in enumerate(sys.stdin):
-        split_line = line.split()
+def print_status():
+    '''
+        Printing the status of the request
+    '''
+    counter = 0
+    size = 0
+    file_size = 0
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
+
+    for l in sys.stdin:
+        line = l.split()
         try:
-            file_size = int(split_line[-1])
-            status_code = split_line[-2]
-        except (IndexError, ValueError):
+            size += int(line[-1])
+            code = line[-2]
+            status_codes[code] += 1
+        except:
             continue
+        if counter == 9:
+            print("File size: {}".format(size))
+            for key, val in sorted(status_codes.items()):
+                if (val != 0):
+                    print("{}: {}".format(key, val))
+            counter = 0
+        counter += 1
+    if counter < 9:
+        print("File size: {}".format(size))
+        for key, val in sorted(status_codes.items()):
+            if (val != 0):
+                print("{}: {}".format(key, val))
 
-        total_file_size += file_size
-        status_codes_count[status_code] += 1
 
-        if (i + 1) % 10 == 0:
-            print("File size: {}".format(total_file_size))
-            for status_code, count in sorted(status_codes_count.items()):
-                if count != 0:
-                    print("{}: {}".format(status_code, count))
-            print()
-
-except KeyboardInterrupt:
-    print("File size: {}".format(total_file_size))
-    for status_code, count in sorted(status_codes_count.items()):
-        if count != 0:
-            print("{}: {}".format(status_code, count)))
+if __name__ == "__main__":
+    print_status()
