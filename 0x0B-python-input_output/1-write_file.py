d1895c4063ab5404684fa695400fd4f212ccee67
bizutilaye <Bizualemtilaye@gmail.com>
2023-04-11 04:40:16 -0700

    Update

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains the function write_file
+"""
+
+
+def write_file(filename="", text=""):
+    """Write text to file filename and return the number of characters written"""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return f.write(text)
+
