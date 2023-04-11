e60fadf96170153309c58b0d7503716f0a06db7d
bizutilaye <Bizualemtilaye@gmail.com>
2023-04-11 01:26:42 -0700

    Update

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,84 +1,23 @@
 #!/usr/bin/python3
-"""Module 11-square.
-
-Creates a Rectangle class and a Square class that inherits from it.
+"""
+more class base
 """
 
 
-class Rectangle:
-    """Class Rectangle.
+Rectangle = __import__('9-rectangle').Rectangle
 
-    Attributes:
-        __width -- width of the rectangle
-        __height -- height of the rectangle
 
-    Methods:
-        __init__(self, width, height)
-        area(self)
-        __str__(self)
-    """
-    def __init__(self, width, height):
-        """Class constructor.
-
-        Args:
-            width -- the width of the rectangle
-            height -- the height of the rectangle
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Method area.
-
-        Returns:
-            The area of the rectangle.
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Method __str__.
-
-        Returns:
-            The following rectangle description: [Rectangle] <width>/<height>
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+"""
+Square class
+"""
 
 
 class Square(Rectangle):
-    """Class Square.
-
-    Attributes:
-        __size -- size of the square
-
-    Methods:
-        __init__(self, size)
-        area(self)
-        __str__(self)
-    """
+    """ Square Class """
     def __init__(self, size):
-        """Class constructor.
-
-        Args:
-            size -- the size of the square
-        """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+        """ size init"""
         self.__size = size
-
-    def area(self):
-        """Method area.
-
-        Returns:
-            The area of the square.
-        """
-        return self.__size ** 2
+        super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        """Method __str__.
-
-        Returns:
-            The following square description: [Square] <size>/<size>
-        """
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
