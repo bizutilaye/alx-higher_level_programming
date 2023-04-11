27a18bf563641f8d1adcc66738ab983b1741fa0f
bizutilaye <Bizualemtilaye@gmail.com>
2023-04-11 06:15:07 -0700

    Update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,39 +1,31 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 import sys
 
-def print_stats():
-    '''
-        Printing statistics after every 10 lines and after keyboard interruption
-    '''
-    counter = 0
-    total_size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
+total_file_size = 0
+status_codes_count = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 
-    for line in sys.stdin:
-        line = line.split()
+try:
+    for i, line in enumerate(sys.stdin):
+        split_line = line.split()
         try:
-            total_size += int(line[-1])
-            code = line[-2]
-            status_codes[code] += 1
-        except:
+            file_size = int(split_line[-1])
+            status_code = split_line[-2]
+        except (IndexError, ValueError):
             continue
-        if counter == 9:
-            print("File size: {}".format(total_size))
-            for key, val in sorted(status_codes.items()):
-                if (val != 0):
-                    print("{}: {}".format(key, val))
-            counter = 0
-        counter += 1
 
-    # printing statistics after keyboard interruption
-    print("File size: {}".format(total_size))
-    for key, val in sorted(status_codes.items()):
-        if (val != 0):
-            print("{}: {}".format(key, val))
+        total_file_size += file_size
+        status_codes_count[status_code] += 1
 
-if __name__ == "__main__":
-    try:
-        print_stats()
-    except KeyboardInterrupt:
-        print_stats()
+        if (i + 1) % 10 == 0:
+            print("File size: {}".format(total_file_size))
+            for status_code, count in sorted(status_codes_count.items()):
+                if count != 0:
+                    print("{}: {}".format(status_code, count))
+            print()
+
+except KeyboardInterrupt:
+    print("File size: {}".format(total_file_size))
+    for status_code, count in sorted(status_codes_count.items()):
+        if count != 0:
+            print("{}: {}".format(status_code, count)))
