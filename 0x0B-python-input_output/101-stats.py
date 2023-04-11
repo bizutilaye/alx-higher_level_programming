e48af46d75bec3395c0dd3e6332419b9d67c3f23
bizutilaye <Bizualemtilaye@gmail.com>
2023-04-11 05:52:00 -0700

    Update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python3
 import sys
 
-
-def print_status():
+def print_stats():
     '''
-        Printing the status of the request
+        Printing statistics after every 10 lines and after keyboard interruption
     '''
     counter = 0
-    size = 0
-    file_size = 0
+    total_size = 0
     status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
 
-    for l in sys.stdin:
-        line = l.split()
+    for line in sys.stdin:
+        line = line.split()
         try:
-            size += int(line[-1])
+            total_size += int(line[-1])
             code = line[-2]
             status_codes[code] += 1
         except:
             continue
         if counter == 9:
-            print("File size: {}".format(size))
+            print("File size: {}".format(total_size))
             for key, val in sorted(status_codes.items()):
                 if (val != 0):
                     print("{}: {}".format(key, val))
             counter = 0
         counter += 1
-    if counter < 9:
-        print("File size: {}".format(size))
-        for key, val in sorted(status_codes.items()):
-            if (val != 0):
-                print("{}: {}".format(key, val))
 
+    # printing statistics after keyboard interruption
+    print("File size: {}".format(total_size))
+    for key, val in sorted(status_codes.items()):
+        if (val != 0):
+            print("{}: {}".format(key, val))
 
 if __name__ == "__main__":
-    print_status()
+    try:
+        print_stats()
+    except KeyboardInterrupt:
+        print_stats()
