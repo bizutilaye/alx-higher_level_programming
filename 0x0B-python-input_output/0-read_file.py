6f5d5469cc5813e41b226577aef3613ebcbd3e46
bizutilaye <Bizualemtilaye@gmail.com>
2023-04-11 04:37:41 -0700

    Update

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for read_file
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, mode='r', encoding='utf-8') as file:
+        print(file.read(), end="")
